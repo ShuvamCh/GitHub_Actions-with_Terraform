c2fd45e31b7c11f15fd29039503f406c8be4cc40
Shuvam Chowdhury <shuvamsvmch@gmail.com>
2024-09-24 15:44:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,10 +19,17 @@ resource "aws_security_group" "example" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress   {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # This allows all outbound traffic.
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
